b2a99fe007bc73ee2975675b039eeb2052a76e15
Diego Parreira <diego.parreira94@gmail.com>
2023-10-19 14:37:22 -0300

    All in one file

--- app.js
+++ app.js
@@ -1,74 +1,144 @@
 const express = require('express');
+const { Sequelize, DataTypes } = require('sequelize');
+const debug = require("debug")("app");
+
 const app = express();
-const sequelize = require('./config/database');
-
-const simpleUser = {
-  "firstName": "Cleo",
-  "lastName": "Cohen",
-  "email": "a@hotmail.edu",
-  "username": "Morbi",
-  "password_hash": "OLI43ACB9RS",
-  "birthdate": "2008-10-14 15:07:22"
-};
-
-const simpleContent = {
-  "title": "odio. Aliquam vulputate",
-  "description": "semper erat, in consectetuer ipsum nunc id enim. Curabitur massa. Vestibulum accumsan neque et nunc. Quisque ornare tortor at risus.",
-  "url": "https://youtube.com",
-  "type": "video",
-  "approved": "0",
-  "userId": 1
-}
-
-const {User, createUserData} = require('./src/models/User');
-const {Content, createContentData} = require('./src/models/Content');
 
 // Importe as classes CRUD para as tabelas aqui
-const {userRouter} = require('./src/routes/users');
+// const {userRouter} = require('./src/routes/users');
 // const conteudosRouter = require('./routes/contents');
 // const comentariosRouter = require('./routes/comments');
 // const materiaisRouter = require('./routes/materials');
 // const categoriasRouter = require('./routes/categories');
 
 // Defina as rotas principais do aplicativo
-app.use('/users', userRouter);
+// app.use('/users', userRouter);
 // app.use('/contents', conteudosRouter);
 // app.use('/comments', comentariosRouter);
 // app.use('/materials', materiaisRouter);
 // app.use('/categories', categoriasRouter);
 
-// Outras configurações e middleware do Express
+const db = new Sequelize('juntos', 'root', '1234', {
+  host: '127.0.0.1',
+  dialect: 'mariadb',
+  port: 3306,
+  logging: debug
+});
 
-// Inicializar o banco de dados
-const init = async () => {
-  try {
-    await sequelize.authenticate(); // Verifique a conexão com o banco de dados
-    await sequelize.sync({ force: true }); // Sincronize os modelos com o banco de dados
-    console.log('Connection with database established successfully');
-
-    try{
-      User.sync();
-      Content.sync();
-  
-      User.hasMany(Content, {foreignKey: 'userId'});
-      Content.belongsTo(User, {foreignKey: 'userId'});
-
-      console.log('Sync and associations with success');
-
-      // User.create(simpleUser);
-      // Content.create(simpleContent);
-    }catch(err){
-      console.log('Error at sync and associate: ' + err);
+const User = db.define('User', {
+  fullName: {
+    type: DataTypes.STRING,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [3, 30],
+        msg: "Error: Name must has 3 charactes and maximun 100."
+      }
     }
-  } catch (error) {
-    console.log('Unable to connect to the database, error: ' + error);
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [3, 30],
+        msg: "Error: Name must has 3 charactes and maximun 100."
+      }
+    }
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: {
+        args: true,
+        msg: "Error: enter a valid email."
+      }
+    }
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isAlphanumeric: {
+        args: true,
+        msg: "Error: enter a alphanumeric password."
+      }
+    }
+  },
+  birthdate: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    validate: {
+      isDate: {
+        args: true,
+        msg: "Error: enter a valid date."
+      }
+    }
+  },
+  type: {
+    type: DataTypes.ENUM('admin', 'mentor', 'student'),
+    defaultValue: 'student'
   }
+}, {
+  hooks: {
+    beforeCreate: (user) => {
+      console.log('Entrou no hook do create');
+      console.log(user.firstName);
+      console.log(user.lastName);
+      user.fullName = `${user.firstName} + ${user.lastName}`;
+    },
+    beforeBulkCreate: (users) => {
+      console.log('Entrou no hook do create');
+      users.map(user => {
+        console.log(user.firstName);
+        console.log(user.lastName);
+        user.fullName = `${user.firstName} ${user.lastName}`;
+      })
+    }
+  }
+});
 
-  // Inicialize o servidor
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Running server at port: ${port}`);
-  });
-}
+const Content = db.define('Content', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  url: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.ENUM('video', 'pdf'),
+    allowNull: false
+  },
+  approved: {
+    type: DataTypes.TINYINT(1),
+    allowNull: false,
+    defaultValue: 0,
+  }
+});
 
-init();
+User.hasMany(Content);
+Content.belongsTo(User);
+
+db.sync({
+  force:true
+});
+
+// Inicialize o servidor
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Running server at port: ${port}`);
+});
